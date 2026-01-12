79d1c3acca38fdb36ca1d849a2f1017dbeff3381
jyotsna70044-jpg <jyotsna70044@gmail.com>
2026-01-13 04:53:32 +0530

    add  loan api

--- myapi/main.py
+++ myapi/main.py
@@ -1,4 +1,4 @@
-# app.py
+# main.py
 from fastapi import FastAPI, Body, Depends
 import schemas
 import models
@@ -8,6 +8,7 @@ from sqlalchemy.orm import Session
 
 Base.metadata.create_all(engine)
 
+
 def get_session():
     session = SessionLocal()
     try:
@@ -15,59 +16,42 @@ def get_session():
     finally:
         session.close()
 
+
 app = FastAPI()
 
-fakeDatabase = {
-    1:{'task':'Clean car'},
-    2:{'task':'Write blog'},
-    3:{'task':'Start stream'},
-}
 
-@app.get("/")
-def getItems(session: Session = Depends(get_session)):
-    items = session.query(models.Item).all()
-    return items
-
-@app.get("/{id}")
-def getItem(id:int, session: Session = Depends(get_session)):
-    item = session.query(models.Item).get(id)
-    return item
-
-#option #1
-# @app.post("/")
-# def addItem(task:str):
-#     newId = len(fakeDatabase.keys()) + 1
-#     fakeDatabase[newId] = {"task":task}
-#     return fakeDatabase
-
-#Option #2
-@app.post("/")
-def addItem(item:schemas.Item, session: Session = Depends(get_session)):
-    item = models.Item(task = item.task)
+@app.post("/loan")
+def addItem(item: schemas.Item, session: Session = Depends(get_session)):
+    item = models.Item(task=item.task)
     session.add(item)
     session.commit()
     session.refresh(item)
 
     return item
 
-#Option #3
-# @app.post("/")
-# def addItem(body = Body()):
-#     newId = len(fakeDatabase.keys()) + 1
-#     fakeDatabase[newId] = {"task":body['task']}
-#     return fakeDatabase
+
+@app.get("/loan")
+def getItems(session: Session = Depends(get_session)):
+    items = session.query(models.Item).all()
+    return items
 
 
-@app.put("/{id}")
-def updateItem(id:int, item:schemas.Item, session: Session = Depends(get_session)):
+@app.get("/loan/{id}")
+def getItem(id: int, session: Session = Depends(get_session)):
+    item = session.query(models.Item).get(id)
+    return item
+
+
+@app.put("/loan/{id}")
+def updateItem(id: int, item: schemas.Item, session: Session = Depends(get_session)):
     itemObject = session.query(models.Item).get(id)
     itemObject.task = item.task
     session.commit()
     return itemObject
 
 
-@app.delete("/{id}")
-def deleteItem(id:int, session: Session = Depends(get_session)):
+@app.delete("/loan/{id}")
+def deleteItem(id: int, session: Session = Depends(get_session)):
     itemObject = session.query(models.Item).get(id)
     session.delete(itemObject)
     session.commit()
